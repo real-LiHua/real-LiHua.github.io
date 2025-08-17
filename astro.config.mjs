01097d699d7c806fbad4e9d531d421dfa7600c6d
Li Hua <lihua@email.com>
2025-08-18 06:34:16 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import llmsGenerate from 'astro-llms-generate';
+import llmsGenerate from "astro-llms-generate";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lihua.codeberg.page',
-  trailingSlash: 'never',
-  integrations: [mdx(), sitemap(), llmsGenerate(), tailwindcss()]
+  site: "https://lihua.codeberg.page",
+  trailingSlash: "never",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [mdx(), sitemap(), llmsGenerate()],
 });
