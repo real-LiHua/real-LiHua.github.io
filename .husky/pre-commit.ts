6aeebff1535b1468d1dc5feeaa32d9649e750d6e
Li Hua <lihua@email.com>
2025-09-21 15:16:12 +0800

    chore: update husky pre-commit hooks
    This commit updates the husky pre-commit hooks to ensure code quality and standards.
    
    Signed-off-by: Li Hua <lihua@email.com>

--- .husky/pre-commit.ts
+++ .husky/pre-commit.ts
@@ -1,5 +1,5 @@
 import { readFile, writeFile } from "node:fs/promises";
-import { applyEdits, modify } from "jsonc-parser";
+import { applyEdits, modify, parse } from "jsonc-parser";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc.js";
 import timezone from "dayjs/plugin/timezone.js";
@@ -8,18 +8,14 @@ console.log("Updating the compatibility date for Wrangler.");
 
 dayjs.extend(utc);
 var config = (await readFile("wrangler.jsonc")).toString();
+var compatibility_date = parse(config)["compatibility_date"];
+var yesterday = dayjs().subtract(1, "day").format("YYYY-MM-DD");
 await writeFile(
   "wrangler.jsonc",
-  applyEdits(
-    config,
-    modify(
-      config,
-      ["compatibility_date"],
-      dayjs().subtract(1, "day").format("YYYY-MM-DD"),
-      {},
-    ),
-  ),
+  applyEdits(config, modify(config, ["compatibility_date"], yesterday, {})),
   {},
 );
-
 console.log("Updated compatibility date for Wrangler.");
+if (compatibility_date !== yesterday) {
+  process.exit(1);
+}
